f92b2df0be4c5299dac19833855dc793d8a261ef
caijiahao <634606228@qq.com>
2016-11-15 12:38:39 +0800

    logback改造

--- src/main/java/com/lida/mongo/controller/MongoController.java
+++ src/main/java/com/lida/mongo/controller/MongoController.java
@@ -1,6 +1,9 @@
 package com.lida.mongo.controller;
 
 import com.lida.mongo.dao.PersonDao;
+import com.lida.mongo.mongoDao.PersonMongoImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,10 +16,13 @@ import javax.annotation.Resource;
 @Controller
 @RequestMapping(value = "/goMongo")
 public class MongoController {
-
-
+    @Resource
+    private PersonMongoImpl personMongo;
+    private static Logger log = LoggerFactory.getLogger(MongoController.class);
     @RequestMapping(value = "/list",method = RequestMethod.GET)
     public String go() {
+
+        log.debug(personMongo.findAll()+"=========================================================================");
         return "mogoList";
     }
 
--- src/test/java/dao/MongoTemplateTest.java
+++ src/test/java/dao/MongoTemplateTest.java
@@ -6,6 +6,8 @@ import com.lida.mongo.entity.Person;
 import com.lida.mongo.mongoDao.PersonMongoImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -19,6 +21,7 @@ import javax.annotation.Resource;
 //告诉junit spring配置文件
 @ContextConfiguration({"classpath:spring/spring-context.xml","classpath:spring/spring-mongo.xml"})
 public class MongoTemplateTest {
+    private static Logger log = LoggerFactory.getLogger(MongoTemplateTest.class);
 
     @Resource
     private PersonMongoImpl personMongo;
@@ -29,6 +32,7 @@ public class MongoTemplateTest {
         personMongo.insertPerson(new Person("weiwei",24,new Address("南王","鑫达路",10)));
         personMongo.removePerson("name3");
         personMongo.updatePerson();
+        log.debug("========================================================");
         System.out.println(personMongo.findAll());
         System.out.println(personMongo.findForRequery("weiwei"));
     }
